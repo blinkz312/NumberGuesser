919aeac666f97821b52284bb4f130c1d4e66b6dc
blinkz312 <chirayurana@yahoo.com>
2023-11-09 12:36:47 +0545

    half completed

--- app/src/main/java/com/blinky/numberguesser/MainActivity.kt
+++ app/src/main/java/com/blinky/numberguesser/MainActivity.kt
@@ -2,14 +2,18 @@ package com.blinky.numberguesser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var userNumber:Int
-        var result:Int
+        val userAns:Int = findViewById<EditText>(R.id.answer).toString().toInt()
 
+        val randomValues = Random.nextInt(1,10)
+        if(userAns == randomValues){
+
+        }
+        }
     }
-    val randomNum = Math.random()
-}
