f179ba98ed2e44851fd73774195833de79f478f2
blinkz312 <chirayurana@yahoo.com>
2023-11-09 12:19:28 +0545

    Initial commit

---
+++ app/src/main/java/com/blinky/numberguesser/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.blinky.numberguesser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var userNumber:Int
+        var result:Int
+
+    }
+    val randomNum = Math.random()
+}
